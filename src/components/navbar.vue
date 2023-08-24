f346b35d013ca6223569365caf89a87c003acd32
Tomer Saada <tomer1594826@gmail.com>
2023-08-24 18:05:52 +0300

    yo

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,13 +2,14 @@
     <div :class="theme">
         <h1>hi</h1>
         <a v-if="pages.length>0" v-for="page in pages" :href="page.link">{{ page.title }}</a>
+        <button @click.prevent="theme == 'light' ? theme = 'dark' : theme = 'light'">hi</button>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const theme = ref('dark')
+const theme = ref("light")
 const pages = ref([
     {
         title:"hi",
@@ -22,17 +23,22 @@ const pages = ref([
 </script>
 
 <style scoped>
+a{
+    color:inherit;
+    margin-right:5px;
+    margin-left:5px;
+}
 div{
     height: 50px;
     display: flex;
     align-items: center;
 }
 .dark {
-    color:white;
+    color:rgb(255, 255, 255);
     background-color:black;
 }
 .light {
     color:black;
-    background-color: white;
+    background-color: rgb(212, 212, 212);
 }
 </style>
