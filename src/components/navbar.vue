64ed728f2ee082e8bfe1a609391400c08993c76e
Tomer Saada <tomer1594826@gmail.com>
2023-08-24 17:31:02 +0300

    updated

---
+++ src/components/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="theme">
+        <h1>hi</h1>
+        <a v-if="pages.length>0" v-for="page in pages" :href="page.link">{{ page.title }}</a>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('dark')
+const pages = ref([
+    {
+        title:"hi",
+        link: "https://www.youtube.com",
+    },
+    {
+        title:"yo",
+        link:"https://google.com",
+    }
+])
+</script>
+
+<style scoped>
+div{
+    height: 50px;
+    display: flex;
+    align-items: center;
+}
+.dark {
+    color:white;
+    background-color:black;
+}
+.light {
+    color:black;
+    background-color: white;
+}
+</style>
