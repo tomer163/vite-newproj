f06a15b958a7fb64bb6a8e6b45a1e36c88b567fb
Tomer Saada <tomer1594826@gmail.com>
2023-08-28 18:26:14 +0300

    g

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+//font-awsome-icons
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faSun } from '@fortawesome/free-solid-svg-icons'
 library.add(faSun)
 
 createApp(App)
+
+//font-awsome-adding-component
 .component('font-awesome-icon', FontAwesomeIcon)
+
 .mount('#app')
