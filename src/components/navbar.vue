ad71b954065b98ff059f3e77283750347e6bcb26
Tomer Saada <tomer1594826@gmail.com>
2023-09-04 01:20:06 +0300

    h

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex items-center px-2 h-14 dark:bg-stone-800 dark:text-white bg-stone-200 drop-shadow-lg mb-5">
+    <div class="min-w-max flex items-center px-2 h-14 dark:bg-stone-800 dark:text-white bg-stone-200 drop-shadow-lg mb-5">
         <h1 class="text-3xl">Tomer Dev</h1>
         <router-link class="m-2 hover:opacity-60" v-for="page in pages" :to="page.slug">{{ page.buttonName }}</router-link>
         <div class="grow"></div>
-        <button class="w-max drop-shadow-xl flex rounded-full bg-stone-100 dark:bg-stone-900 text-inherit py-1.5 px-2.5 hover:opacity-80 hover:ease-in duration-100 transition ease-in" @click.prevent="toggleDark()">
+        <button class="drop-shadow-xl flex rounded-full bg-stone-100 dark:bg-stone-900 text-inherit py-1.5 px-2.5 hover:opacity-80 hover:ease-in duration-100 transition ease-in" @click.prevent="toggleDark()">
             <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke-width="2" stroke="yellow" class="hidden dark:block w-6 h-6 mr-2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
             </svg>
@@ -17,7 +17,6 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
@@ -29,9 +28,6 @@ const props = defineProps({
         defalult: [], 
     },
 })
-
-
-const theme = ref("light")
 </script>
 
 <style scoped>
