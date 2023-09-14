afe670eddd4c24f885d3bd26e606729d4e2a4cc2
Tomer Saada <tomer1594826@gmail.com>
2023-09-14 22:06:58 +0300

    newinput

--- src/components/textInput.vue
+++ src/components/textInput.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="relative dark:text-stone-500 text-stone-500">
-        <span :class="hasError ? 'text-red-500' : focused ? 'text-sky-500' : '' , focused ? '-translate-y-2 translate-x-8 text-sm' : modelValue == '' ? 'translate-y-3 translate-x-6' : '-translate-y-2 translate-x-8 text-sm'" class="absolute select-none pointer-events-none rounded-full dark:bg-stone-900 bg-stone-100 px-1 transition-all ease-out">
+        <input type="text" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" :class="hasError ? 'focus:border-red-500 focus:dark:border-red-500 dark:border-red-500 border-red-500': 'dark:border-stone-700 border-stone-500'" class="peer text-stone-400 px-5 py-2 bg-inherit border focus:border-sky-500 focus:dark:border-sky-500 focus:outline-none block w-full h-[48px] rounded-full transition-all ease-out"/>
+        <span :class="hasError ? 'text-red-500 dark:text-red-500 peer-focuse:text-red-500 peer-focuse:dark:text-red-500' : 'peer-focus:text-sky-500' , modelValue == '' ? '-translate-y-9 translate-x-6' : '-translate-y-14 translate-x-8 text-sm'" class="absolute select-none pointer-events-none peer-focus:-translate-y-14 peer-focus:translate-x-8 peer-focus:text-sm rounded-full dark:bg-stone-900 bg-stone-100 px-1 transition-all ease-out">
             <slot></slot>
         </span>
-        <input ref="target" type="text" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" :class="hasError ? 'border-red-500': focused ? 'border-sky-500' : 'dark:border-stone-700 border-stone-500'" class="text-stone-400 px-5 py-2 bg-inherit border focus:outline-none block w-full h-[48px] rounded-full transition-all ease-out"/>
     </div>
 </template>
 
 <script setup>
-import { ref } from'vue'
-import { useFocus } from '@vueuse/core'
 
 const props = defineProps({
     hasError: {
@@ -18,9 +16,6 @@ const props = defineProps({
     },
     modelValue: String
 })
-
-const target = ref()
-const { focused } = useFocus(target)
 
 const emit = defineEmits(['update:modelValue'])
 </script>
